71184a4933b6a067fb0c0e133094b83d7bc72f79
bkgriffin <4809837+bkgriffin@users.noreply.github.com>
2023-11-04 13:59:48 -0400

    Added divide() method.

--- calculator.js
+++ calculator.js
@@ -13,6 +13,11 @@ function multiply(a, b) {
     return a * b;
 }
 
+// Divide one number by another.
+function divide(a, b) { 
+    return a / b;
+}
+
 // Testing add()
 console.log('------------------ TESTING add() ------------------');
 console.log('4 + 5 = ' + add(4,5) + ' (Expected Value: 9)');
@@ -41,4 +46,14 @@ console.log('-3 * 4 = ' + multiply(-3,4) + ' (Expected Value: -12)');
 console.log('-4 * -5 = ' + multiply(-4,-5) + ' (Expected Value: 20)');
 console.log('0 * 0 = ' + multiply(0,0) + ' (Expected Value: 0)');
 console.log('0 * -5 = ' + multiply(0,-5) + ' (Expected Value: 0)');
+console.log('---------------------------------------------------');
+
+// Testing divide()
+console.log('---------------- TESTING divide() ---------------');
+console.log('4 / 8 = ' + divide(4,8) + ' (Expected Value: 0.5)');
+console.log('0 / 5 = ' + divide(12,3) + ' (Expected Value: 4)');
+console.log('-3 / 4 = ' + divide(-4,4) + ' (Expected Value: -1)');
+console.log('0 / -5 = ' + divide(0,-5) + ' (Expected Value: 0)');
+console.log('0 / 0 = ' + divide(0,0) + ' (Expected Value: NaN)');
+console.log('0 / -5 = ' + divide(1,3) + ' (Expected Value: 0.33)');
 console.log('---------------------------------------------------');
